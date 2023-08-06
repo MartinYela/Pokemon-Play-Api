c8530a6575ba6a441cac26f9641409fd3553d4d5
MartinYela <martinyela@hotmail.com>
2023-08-06 15:30:39 +0200

    fix: create user in bbdd

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,17 @@
-import {db} from '../db/connection.js';
+import { db } from "../db/connection.js";
+import { User } from "../user/user.model.js";
 
-export async function createTable( ) {
-    await db.sync()
-        .then(() => console.log('Conectado al Servidor'))
-        .catch(error => console.log(error));
+export async function createTable() {
+  await db
+    .sync()
+    .then(() => console.log("Conectado al Servidor"))
+    .catch((error) => console.log(error));
+  // Esto no debería ir, pero como no me dió tiempo a implementar el registro en el front, lo incluyo para que cuando levanten el proyecto tengan un usuario con el que empezar. El endpoint del registro si está implementado, enviando email en el body, a /user/createUser (POST), y la contraseña la envía automáticamente al email enviado (por si quereis probar por postman)
+  User.create({
+    email: "user@hotmail.com",
+    password:
+      "fde131b9b282936f0f6fe3a38b1416ebb2c9ac7b59109b218444e26bb99ced0e437baa74d222d5e75b56d5262988066355804b517b5f8dd0b28062bdf3366ca6",
+  })
+    .then((c) => console.log("userCreate OK"))
+    .catch((error) => console.log(error));
 }
--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,47 +1,47 @@
 import { User } from "./user.model.js";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { encodePassword } from "../auth/auth.controller.js";
 import { transporter } from "../config/mailer.js";
 
 export async function createUserCtrl(req, res) {
-    const { email } = req.body;
-    const password = uuidv4();
-    try {
-        const usuario = await User.create({
-            email: email,
-            password: encodePassword(password),
-        })
+  const { email } = req.body;
+  const password = uuidv4();
+  try {
+    const usuario = await User.create({
+      email: email,
+      password: encodePassword(password),
+    });
 
-        try {
-            let info = await transporter.sendMail({
-                from: '"Envío de password" <appfotograph@gmail.com>', // sender address
-                to: email, // list of receivers
-                subject: "Completar registro Pokemon Play", // Subject line
-                html: `
+    try {
+      let info = await transporter.sendMail({
+        from: '"Envío de password" <appfotograph@gmail.com>', // sender address
+        to: email, // list of receivers
+        subject: "Completar registro Pokemon Play", // Subject line
+        html: `
                 <h4>Has sido añanadido como usuario en <b>Pokemon play</b></h4>
                 <p>Tu password es: ${password}</p>
                 <p>Por favor, haz click en el siguiente enlace para completar el registro:</p>
                 <a href='http://localhost:3000'>http://localhost:3000</a>
-            `, 
-            });
-        } catch (error) {
-            res.status(400).json({
-                status: false,
-                error: 'No se ha podido enviar mail',
-                data: error
-            })
-        }
-
-        res.status(201).json({
-            status: true,
-            error: null,
-            data: usuario.email
-        })
+            `,
+      });
     } catch (error) {
-        res.status(404).json({
-            status: false,
-            error: error,
-            data: null
-        })
+      res.status(400).json({
+        status: false,
+        error: "No se ha podido enviar mail",
+        data: error,
+      });
     }
+
+    res.status(201).json({
+      status: true,
+      error: null,
+      data: usuario.email,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: false,
+      error: error,
+      data: null,
+    });
+  }
 }
